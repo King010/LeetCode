8884258e28845a7bebca0eff3160281876e8e019
dingtalk_vmpard_xJ5If <zhangtengfei@kkworld.com>
2024-03-29 19:03:34 +0800

    firstcommit

---
+++ testMacOS/testMacOS/用栈实现队列.swift
@@ -0,0 +1,28 @@
+//
+//  用栈实现队列.swift
+//  testMacOS
+//
+//  Created by 腾飞张 on 2024/3/29.
+//
+
+import Cocoa
+
+class MyQueue {
+    
+    var stack1 = [Int]()
+    var stack2 = [Int]()
+
+    init() {}
+
+    func push(_ x: Int) {
+        
+    }
+
+    func pop() -> Int {
+        
+    }
+
+    func peek() -> Int {}
+
+    func empty() -> Bool {}
+}
